5b5738c58ac1a1d6b34df953e703043cb962f61e
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-01-27 16:50:42 +1100

    Updated README and added bash utility functions
    The aim is that these functions can be curled into the docker container

--- README.md
+++ README.md
@@ -12,6 +12,20 @@ This repository is a personal collection of useful actions tailored to my person
 
 As GitHub actions does not support stages like GitLab, the `jobs.<job_id>.needs` keyword is used. Reference: [Workflow syntax for GitHub Actions](https://help.github.com/en/actions/automating-your-workflow-with-github-actions/workflow-syntax-for-github-actions#jobsjob_idneeds)
 
+Icons are from [Feather](https://help.github.com/en/actions/automating-your-workflow-with-github-actions/metadata-syntax-for-github-actions#icon).
+
+## Features
+
+1. lint
+    - [python](https://github.com/AlwinW/actions-lint-python)
+        - black, pycodestyle, flake8, pylint, isort, mypy
+        - Will autoformat and use git diff to show results
+2. build
+3. quality
+    * [bandit](https://github.com/PyCQA/bandit)
+4. test
+5. deploy
+
 ## Standard CI
 
 Consider Print default variables for debugging
@@ -19,4 +33,4 @@ Consider Print default variables for debugging
 ## References
 
 * [Publishing Actions in Github Marketplace](https://help.github.com/en/actions/automating-your-workflow-with-github-actions/publishing-actions-in-github-marketplace)
-* [Workflow syntax for GitHub Actions](https://help.github.com/en/actions/automating-your-workflow-with-github-actions/workflow-syntax-for-github-actions#jobsjob_idneeds)
+* [Workflow syntax for GitHub Actions](https://help.github.com/en/actions/automating-your-workflow-with-github-actions/workflow-syntax-for-github-actions)
---
+++ utils/utils.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+
+# Functions for pretty printing
+
+function pretty_print() {
+    if [[ $2 -eq -1 ]]
+    then
+        echo -e "\n"
+        local MESSAGE="Running $1 Test"
+        local COLOUR=6
+        elif [[ $2 -eq 0 ]]; then
+        local MESSAGE="$1 Test Passed!"
+        local COLOUR=2
+        elif [[ $2 -eq 1 ]]; then
+        local MESSAGE="$1 Test Failed!"
+        local COLOUR=1
+    else
+        echo -e "\n"
+        local MESSAGE="$1"
+        local COLOUR=6
+    fi
+    echo -e "\e[3${COLOUR}m##########     \e[4${COLOUR}m\e[30m ${MESSAGE} \e[49m     \e[3${COLOUR}m##########\e[0m"
+}
+
+function header_print() {
+    echo -e "\e[34m############################################################\e[0m"
+    echo -e "\e[1mStarting ${GITHUB_WORKFLOW}\e[0m"
+    echo -e "\e[34mFrom ${GITHUB_ACTION}\e[0m"
+    echo -e "\e[34mRunning $0\e[0m"
+    echo -e "\e[34m------------------------------------------------------------\e[0m"
+}
+
+function footer_print() {
+    echo -e "\n"
+    echo -e "\e[34m------------------------------------------------------------\e[0m"
+    echo -e "\e[1mCompleted ${GITHUB_WORKFLOW}\e[0m"
+    echo -e "\e[34mFrom ${GITHUB_ACTION}\e[0m"
+    echo -e "\e[34m############################################################\e[0m"
+    
+}
+
+function test_print() {
+    echo Current terminal: $TERM
+    echo $PWD
+    
+    echo -e "Normal \e[1mBold\e[0m"
+    echo -e "Normal \e[2mDim\e[0m"
+    echo -e "Normal \e[4mUnderlined\e[0m"
+    echo -e "Normal \e[5mBlink\e[0m"
+    echo -e "Normal \e[7minverted\e[0m"
+    echo -e "Normal \e[8mHidden\e[0m"
+    
+    echo -e "Default \e[39mDefault\e[0m"
+    echo -e "Default \e[30mBlack\e[0m"
+    echo -e "Default \e[31mRed\e[0m"
+    echo -e "Default \e[32mGreen\e[0m"
+    echo -e "Default \e[33mYellow\e[0m"
+    echo -e "Default \e[34mBlue\e[0m"
+    echo -e "Default \e[35mMagenta\e[0m"
+    echo -e "Default \e[36mCyan\e[0m"
+    echo -e "Default \e[37mLight gray\e[0m"
+    echo -e "Default \e[90mDark gray\e[0m"
+    echo -e "Default \e[91mLight red\e[0m"
+    echo -e "Default \e[92mLight green\e[0m"
+    echo -e "Default \e[93mLight yellow\e[0m"
+    echo -e "Default \e[94mLight blue\e[0m"
+    echo -e "Default \e[95mLight magenta\e[0m"
+    echo -e "Default \e[96mLight cyan\e[0m"
+    echo -e "Default \e[97mWhite\e[0m"
+    
+    echo -e "Default \e[49mDefault\e[0m"
+    echo -e "Default \e[40mBlack\e[0m"
+    echo -e "Default \e[41mRed\e[0m"
+    echo -e "Default \e[42mGreen\e[0m"
+    echo -e "Default \e[43mYellow\e[0m"
+    echo -e "Default \e[44mBlue\e[0m"
+    echo -e "Default \e[45mMagenta\e[0m"
+    echo -e "Default \e[46mCyan\e[0m"
+    echo -e "Default \e[47mLight gray\e[0m"
+    echo -e "Default \e[100mDark gray\e[0m"
+    echo -e "Default \e[101mLight red\e[0m"
+    echo -e "Default \e[102mLight green\e[0m"
+    echo -e "Default \e[103mLight yellow\e[0m"
+    echo -e "Default \e[104mLight blue\e[0m"
+    echo -e "Default \e[105mLight magenta\e[0m"
+    echo -e "Default \e[106mLight cyan\e[0m"
+    echo -e "Default \e[107mWhite\e[0m"
+}
